af21d9a29144477f20037f25a63af0fc4899e61a
Egor <tatyana.orehva@mail.ru>
2020-06-01 12:08:20 +0300

    второе и третье задания

---
+++ eclipse-workspace/Tanya/REAMDME.MD/src/modul1/Task2.java
@@ -0,0 +1,13 @@
+package modul1;
+
+public class Task2 {
+	public static void main(String[] args) {
+		int n;
+		n = 325;
+		int s = n + n;
+
+		System.out.println(s);
+
+	}
+
+}
---
+++ eclipse-workspace/Tanya/REAMDME.MD/src/modul1/Task3.java
@@ -0,0 +1,12 @@
+package modul1;
+
+public class Task3 {
+	public static void main(String[] args) {
+		double num = 5.25;
+
+		System.out.println(Math.round(5.25));
+		System.out.println(Math.floor(5.25));
+		System.out.println(Math.ceil(5.25));
+	}
+
+}
